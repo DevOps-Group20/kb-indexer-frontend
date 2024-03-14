e906e5c70458ad78eea11d06e9449ffac2ec7594
Daniel Volpin <Daniel.Volpin@ah.nl>
2024-03-14 16:41:23 +0100

    D2-38 Refactored code.
    Signed-off-by: Daniel Volpin <Daniel.Volpin@ah.nl>

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,13 +1,9 @@
 <template>
-  <div class="container fill-height d-flex align-center">
-    <div class="responsive text-center">
-      <div class="row limit-row-width">
-        <div class="col-12 col-sm-8 offset-sm-2">
-          <div class="mt-5 has-text-weight-bold">
-            <div class="toolbar-title headline text-uppercase is-align-content-center is-flex is-justify-content-center">
-              <span class="font-weight-bold">Login to Index Database Sources</span>
+    <div class="is-responsive is-center">
+      <div class="limit-row-width">
+            <div class="mt-5 is-align-content-center is-flex is-justify-content-center">
+              <span class="has-text-weight-bold">Login to Index Database Sources</span>
             </div>
-          </div>
           <div class="spacer"></div>
           <div>
             <b-field label="Username">
@@ -21,10 +17,8 @@
           <div class="is-flex is-justify-content-center">
             <ActionButton type="is-primary" :buttonAction="login">Login</ActionButton>
           </div>
-        </div>
       </div>
     </div>
-  </div>
 </template>
 
 <script setup lang="ts">
