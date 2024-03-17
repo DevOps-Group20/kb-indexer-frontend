dcb5db87975324f4117b9de52e8401c601f09515
Daniel Verner <danielv2000@gmail.com>
2024-03-18 00:00:47 +0100

    fix auth logout issue

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import { getAuth } from "firebase/auth";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 export const router = createRouter({
   history: createWebHistory(),
@@ -18,12 +18,28 @@ export const router = createRouter({
 
 router.beforeEach((to, from, next) => {
   const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
-  const auth = getAuth();
-  const user = auth.currentUser;
 
-  if (requiresAuth && !user) {
-    next('/login');
-  } else {
+  if (!requiresAuth) {
     next();
+    return;
   }
+
+  const auth = getAuth();
+  new Promise((resolve, reject) => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      unsubscribe();
+      resolve(user);
+    }, reject);
+  })
+    .then(user => {
+      if (!user) {
+        next('/login');
+      } else {
+        next();
+      }
+    })
+    .catch(error => {
+      console.error(error);
+      next(false);
+    });
 });
